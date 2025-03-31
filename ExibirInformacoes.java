37cfae7c07094335fa3723ddb1e012e299846642
danielhenrique <165221001+dhenriquea1419@users.noreply.github.com>
2025-03-31 19:50:09 -0300

    Create ExibirInformacoes.java

---
+++ ExibirInformacoes.java
@@ -0,0 +1,6 @@
+public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: " + preco);
+        System.out.println("Quantidade em estoque: " + quantidadeEstoque);
+    }
+}
